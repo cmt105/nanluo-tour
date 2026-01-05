52944d4081e88143393c3c9e7725226552147ab8
chenmantong <3337409865@qq.com>
2026-01-05 22:21:18 +0800

    feat: 美化系统界面并优化页面展示

--- miniprogram/pages/list/list.js
+++ miniprogram/pages/list/list.js
@@ -1,21 +1,15 @@
-// pages/list/list.js
+const db = wx.cloud.database();
 Page({
-  data: {
-    poiList: []
-  },
+  data: { pois: [] },
   onLoad() {
-    // 基础库 3.13.1 已自动开启云开发环境 [cite: 227]
-    this.loadPOIs();
-  },
-  loadPOIs() {
-    const db = wx.cloud.database();
-    // 确保你在云开发控制台已经创建了名为 'pois' 的集合 [cite: 232, 233]
+    wx.showLoading({ title: '加载中...' });
     db.collection('pois').get().then(res => {
-      this.setData({ 
-        poiList: res.data 
-      }); 
+      console.log("获取到的数据：", res.data); // 调试用
+      this.setData({ pois: res.data });
+      wx.hideLoading();
     }).catch(err => {
-      console.error("数据库读取失败，请检查是否创建了 pois 集合并设置了权限", err);
+      console.error("数据库获取失败", err);
+      wx.hideLoading();
     });
   }
 })
